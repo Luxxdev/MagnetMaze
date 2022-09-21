3ce80bfa2a244b0acd1f101d172589f58c8e4713
gigiofig <gfps2011@gmail.com>
2022-09-20 22:50:10 -0300

    adjustments to player script and main scene

--- MagnetMaze/Assets/Scripts/PlayerScript.cs
+++ MagnetMaze/Assets/Scripts/PlayerScript.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 8;
-    private float jumpingPower = 16;
-    private bool isFacingRight = true;
+    private float speed = 3;
+    private float jumpingPower = 5;
     private bool isToolActive = false;
     private bool currentPole = false;
+    private bool canInteract = false;
+    private List<GameObject> objects = new List<GameObject>();
     [SerializeField] private GameObject tool;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
     
-   /* void Start()
+   void Start()
     {
-       private GameObject tool
-       tool = this.gameObject.transform.GetChild(1).GetChild(0).gameObject;
-    }*/
+     //  private GameObject tool = this.gameObject.transform.GetChild(1).GetChild(0).gameObject;
+    }
 
 
     // Update is called once per frame
@@ -31,16 +31,47 @@ public class PlayerScript : MonoBehaviour
         if (Input.GetButtonDown("ToggleTool"))
         {
             currentPole = !currentPole;
+            if (currentPole && isToolActive)
+            {
+                this.gameObject.layer = LayerMask.NameToLayer("Positive");
+                tool.GetComponent<SpriteRenderer>().color = new Color (1,0,0,0.3f);
+            }
+            else if (!currentPole && isToolActive)
+            {
+                this.gameObject.layer = LayerMask.NameToLayer("Negative");
+                tool.GetComponent<SpriteRenderer>().color = new Color (0,0,1,0.3f);
+            }
+
         }
 
         if (Input.GetButtonDown("ActivateTool"))
         {
             isToolActive = !isToolActive;
+            tool.GetComponent<SpriteRenderer>().enabled = isToolActive;
+            if (currentPole && isToolActive)
+            {
+                this.gameObject.layer = LayerMask.NameToLayer("Positive");
+                tool.GetComponent<SpriteRenderer>().color = new Color (1,0,0,0.3f);
+            }
+            else if (!currentPole && isToolActive)
+            {
+                this.gameObject.layer = LayerMask.NameToLayer("Negative");
+                tool.GetComponent<SpriteRenderer>().color = new Color (0,0,1,0.3f);
+            }
+            else if (!isToolActive)
+            {
+                this.gameObject.layer = LayerMask.NameToLayer("Default");
+            }
         }
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonDown("Interact") && canInteract)
+        {
+            Debug.Log("interacting");
         }
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
@@ -60,15 +91,36 @@ public class PlayerScript : MonoBehaviour
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
-
-    private void Flip() // check if inspector's flip works instead of the function
+    private void Flip()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        if (horizontal > 0f)
         {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale; 
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (horizontal < 0f)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer >= 7 && other.gameObject.layer <= 9)
+        {
+            canInteract = true;
+            objects.Add(other.gameObject);
+            Debug.Log(canInteract + " " + objects);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.layer >= 7 && other.gameObject.layer <= 9)
+        {
+            canInteract = false;
+            objects.Remove(other.gameObject);
+            Debug.Log(canInteract);
         }
     }
 }
