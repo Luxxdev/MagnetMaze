f3432b441a7a8d91c9afb024b78d4c08d9391db9
Murilo Gama <53795576+Murilovsky@users.noreply.github.com>
2022-09-22 21:31:37 -0300

    Animations

--- MagnetMaze/Assets/Scripts/PlayerScript.cs
+++ MagnetMaze/Assets/Scripts/PlayerScript.cs
@@ -4,142 +4,147 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    private float horizontal;
-    private float speed = 3;
-    private float jumpingPower = 5;
-    private bool isFacingRight = true;
-    private bool isToolActive = false;
-    private bool currentPole = false;
-    private bool canInteract = false;
-    private List<GameObject> objects = new List<GameObject>();
-    [SerializeField] private GameObject tool;
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
+   private float horizontal;
+   private float speed = 3;
+   private float jumpingPower = 5;
+   private bool isFacingRight = true;
+   private bool isToolActive = false;
+   private bool currentPole = false;
+   private bool canInteract = false;
+   private List<GameObject> objects = new List<GameObject>();
+   [SerializeField] protected Animator anim;
+   [SerializeField] private GameObject tool;
+   [SerializeField] private Rigidbody2D rb;
+   [SerializeField] private Transform groundCheck;
+   [SerializeField] private LayerMask groundLayer;
 
-    
    void Start()
-    {
-     //  private GameObject tool = this.gameObject.transform.GetChild(1).GetChild(0).gameObject;
-    }
+   {
+      //  private GameObject tool = this.gameObject.transform.GetChild(1).GetChild(0).gameObject;
+   }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontal = Input.GetAxisRaw("Horizontal");
+   // Update is called once per frame
+   void Update()
+   {
+      horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("ToggleTool"))
-        {
-            currentPole = !currentPole;
-            if (currentPole && isToolActive)
+      if (Input.GetButtonDown("ToggleTool"))
+      {
+         anim.SetTrigger("toolClick");
+         currentPole = !currentPole;
+         if (currentPole && isToolActive)
+         {
+            this.gameObject.layer = LayerMask.NameToLayer("ToolPositive");
+            tool.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 0.3f);
+         }
+         else if (!currentPole && isToolActive)
+         {
+            this.gameObject.layer = LayerMask.NameToLayer("ToolNegative");
+            tool.GetComponent<SpriteRenderer>().color = new Color(0, 0, 1, 0.3f);
+         }
+
+      }
+
+      if (Input.GetButtonDown("ActivateTool"))
+      {
+         anim.SetTrigger("toolOn");
+         isToolActive = !isToolActive;
+         tool.GetComponent<SpriteRenderer>().enabled = isToolActive;
+         tool.GetComponent<CapsuleCollider2D>().enabled = isToolActive;
+         if (currentPole && isToolActive)
+         {
+            this.gameObject.layer = LayerMask.NameToLayer("ToolPositive");
+            tool.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 0.3f);
+         }
+         else if (!currentPole && isToolActive)
+         {
+            this.gameObject.layer = LayerMask.NameToLayer("ToolNegative");
+            tool.GetComponent<SpriteRenderer>().color = new Color(0, 0, 1, 0.3f);
+         }
+         else if (!isToolActive)
+         {
+            this.gameObject.layer = LayerMask.NameToLayer("Player");
+         }
+      }
+
+      if (Input.GetButtonDown("Jump") && IsGrounded())
+      {
+         rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+      }
+
+      if (Input.GetButtonDown("Interact") && canInteract)
+      {
+         if (objects[0].layer == 7 || objects[0].layer == 8)
+         {
+            objects[0].layer = 9;
+         }
+         else if (objects[0].layer == 9)
+         {
+            if (!currentPole)
             {
-                this.gameObject.layer = LayerMask.NameToLayer("ToolPositive");
-                tool.GetComponent<SpriteRenderer>().color = new Color (1,0,0,0.3f);
+               objects[0].layer = 8;
+               Debug.Log(objects[0] + " " + "negativou");
             }
-            else if (!currentPole && isToolActive)
+            else if (currentPole)
             {
-                this.gameObject.layer = LayerMask.NameToLayer("ToolNegative");
-                tool.GetComponent<SpriteRenderer>().color = new Color (0,0,1,0.3f);
+               objects[0].layer = 7;
+               Debug.Log(objects[0] + " " + "positivou");
             }
+         }
+         Debug.Log("interacting");
+      }
 
-        }
+      if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+      {
+         anim.SetFloat("Yvelocity", rb.velocity.y);
+         rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+      }
 
-        if (Input.GetButtonDown("ActivateTool"))
-        {
-            isToolActive = !isToolActive;
-            tool.GetComponent<SpriteRenderer>().enabled = isToolActive;
-            tool.GetComponent<CapsuleCollider2D>().enabled = isToolActive;
-            if (currentPole && isToolActive)
-            {
-                this.gameObject.layer = LayerMask.NameToLayer("ToolPositive");
-                tool.GetComponent<SpriteRenderer>().color = new Color (1,0,0,0.3f);
-            }
-            else if (!currentPole && isToolActive)
-            {
-                this.gameObject.layer = LayerMask.NameToLayer("ToolNegative");
-                tool.GetComponent<SpriteRenderer>().color = new Color (0,0,1,0.3f);
-            }
-            else if (!isToolActive)
-            {
-                this.gameObject.layer = LayerMask.NameToLayer("Player");
-            }
-        }
+      Flip();
+   }
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
+   private void FixedUpdate()
+   {
+      anim.SetFloat("Yvelocity", rb.velocity.y);
+      anim.SetBool("isOnFloor", rb.velocity.y != 0 ? false : true);
+      anim.SetBool("isMoving", rb.velocity.x != 0 ? true : false);
 
-        if (Input.GetButtonDown("Interact") && canInteract)
-        {
-            if (objects[0].layer == 7 || objects[0].layer == 8)
-            {
-                objects[0].layer = 9;
-            }
-            else if (objects[0].layer == 9)
-            {
-                if (!currentPole)
-                {
-                    objects[0].layer = 8;
-                    Debug.Log(objects[0] + " " + "negativou");
-                }
-                else if (currentPole)
-                {
-                    objects[0].layer = 7;
-                    Debug.Log(objects[0] + " " + "positivou");
-                }
-            }
-            Debug.Log("interacting");
-        }
+      rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+   }
 
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
+   private bool IsGrounded()
+   {
+      return Physics2D.OverlapBox(groundCheck.position, new Vector2(0.1f, 0.02f), 0, groundLayer);
+      //return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+   }
+   private void Flip()
+   {
+      if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+      {
+         isFacingRight = !isFacingRight;
+         Vector3 localScale = transform.localScale;
+         localScale.x *= -1f;
+         transform.localScale = localScale;
+      }
+   }
 
-        Flip();
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapBox(groundCheck.position, new Vector2(0.1f,0.02f), 0, groundLayer);
-        //return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
-    }
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale; 
-        }
-    }
-    /// Sent when an incoming collider makes contact with this object's
-    /// collider (2D physics only).
-    /// <param name="other">The Collision2D data associated with this collision.</param>
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.layer >= 7 && other.gameObject.layer <= 9)
-        {
-            canInteract = true;
-            objects.Add(other.gameObject);
-            Debug.Log(canInteract + " " + objects);
-        }
-    }
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.layer >= 7 && other.gameObject.layer <= 9)
-        {
-            canInteract = false;
-            objects.Remove(other.gameObject);
-            Debug.Log(canInteract);
-        }
-    }
+   private void OnCollisionEnter2D(Collision2D other)
+   {
+      if (other.gameObject.layer >= 7 && other.gameObject.layer <= 9)
+      {
+         canInteract = true;
+         objects.Add(other.gameObject);
+         Debug.Log(canInteract + " " + objects);
+      }
+   }
+   private void OnCollisionExit2D(Collision2D other)
+   {
+      if (other.gameObject.layer >= 7 && other.gameObject.layer <= 9)
+      {
+         canInteract = false;
+         objects.Remove(other.gameObject);
+         Debug.Log(canInteract);
+      }
+   }
 }
