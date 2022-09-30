d13b8198a46ec5ca9237b7de120caf0aabb07ab2
Murilo Gama <53795576+Murilovsky@users.noreply.github.com>
2022-09-29 23:13:15 -0300

    Diálogo básico feito

--- MagnetMaze/Assets/Scripts/BottomTextManagement.cs
+++ MagnetMaze/Assets/Scripts/BottomTextManagement.cs
@@ -2,46 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class BottomTextManagement : MonoBehaviour
 {
    [SerializeField]
-   private List<string> frases;
+   public List<string> frases;
+   public Image charExpression;
+   public Image explanationImage;
+   protected int phraseIndex = 0;
+   protected float transTime = 0.5f;
    [SerializeField]
    protected RectTransform panelTransform;
+   public TMPro.TextMeshProUGUI textDisplay;
    protected Dictionary<string, Vector2> positions = new Dictionary<string, Vector2>(){
     {"hidden", new Vector2(0, -178)},
     {"onScreen", new Vector2(0, 0)}
    };
-   protected
-   // Start is called before the first frame update
-   void Start()
-   {
-      panelTransform.anchoredPosition = positions["hidden"];
-   }
 
+   public void Start()
+   {
+      textDisplay.text = frases[0];
+   }
    public void CallDialog()
    {
-      panelTransform.anchoredPosition = positions["onScreen"];
+      panelTransform.DOAnchorPosY(positions["onScreen"].y, transTime);
+      explanationImage.DOFade(1, 0.8f);
+      charExpression.DOFade(1, 0.8f);
+      ResetPhrases();
    }
    public void CloseDialog()
    {
-      panelTransform.anchoredPosition = positions["hidden"];
-      // Update is called once per frame
+      panelTransform.DOAnchorPosY(positions["hidden"].y, transTime);
+      charExpression.DOFade(0, 0.2f);
+      explanationImage.DOFade(0, 0.2f);
+      textDisplay.text = "";
+   }
+
+   public void NextPhrase()
+   {
+      phraseIndex++;
+      if (phraseIndex >= frases.Count)
+      {
+         CloseDialog();
+      }
+      textDisplay.text = frases[phraseIndex];
+   }
+   public void ResetPhrases()
+   {
+      phraseIndex = 0;
+      textDisplay.text = frases[phraseIndex];
    }
    void Update()
    {
 
-   }
-   IEnumerator Tween(Vector3 targetPosition, float moveDuration = 1.0f)
-   {
-      Vector3 previousPosition = gameObject.transform.position;
-      float time = 0.0f;
-      do
-      {
-         time += Time.deltaTime;
-         gameObject.transform.position = Vector3.Lerp(previousPosition, targetPosition, time / moveDuration);
-         yield return 0;
-      } while (time < moveDuration);
    }
 }
