f402260dde4bb5270f8d20ae2e90b798a320bf34
Lucas Queirolo dos Santos <lq.santos@hotmail.com>
2022-10-04 16:41:28 -0300

    conflic bugfix

--- MagnetMaze/Assets/Scripts/Plug.cs
+++ MagnetMaze/Assets/Scripts/Plug.cs
@@ -12,6 +12,14 @@ public class Plug : MonoBehaviour
         }
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Box") && !collision.isTrigger && !collision.gameObject.GetComponent<MagnetBox>().conducting)
+        {
+            collision.gameObject.GetComponent<MagnetBox>().conducting = true;
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Box") && !collision.isTrigger)
