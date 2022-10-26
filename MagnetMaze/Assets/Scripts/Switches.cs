4ab59a7f942a35f4c986fc5b72fb769d083b92a7
Murilo Gama <53795576+Murilovsky@users.noreply.github.com>
2022-10-26 17:26:45 -0300

    Fios com LineRenderer, suporte multi-interaction

--- MagnetMaze/Assets/Scripts/Switches.cs
+++ MagnetMaze/Assets/Scripts/Switches.cs
@@ -4,37 +4,43 @@ using UnityEngine;
 
 public abstract class Switches : MonoBehaviour
 {
-    [SerializeField] protected GameObject interactableObject;
-    [SerializeField] protected SpriteRenderer spriteRenderer;
-    [SerializeField] protected Sprite[] sprite;
-    protected float energyRequired;
-    protected bool canActivate = true;
-    protected bool hasBattery = false;
-    [SerializeField] protected float steps = 1.0f;
-    protected float stepsCount;
-    protected float energy = 0;
+   [SerializeField] protected GameObject[] interactableObject;
+   [SerializeField] protected SpriteRenderer spriteRenderer;
+   [SerializeField] protected Sprite[] sprite;
+   protected float energyRequired;
+   protected bool canActivate = true;
+   protected bool hasBattery = false;
+   [SerializeField] protected float steps = 1.0f;
+   protected float stepsCount;
+   protected float energy = 0;
 
-    protected void Awake()
-    {
-        if (interactableObject.CompareTag("Battery"))
-        {
+   protected void Awake()
+   {
+      foreach (var item in interactableObject)
+      {
+         if (item.CompareTag("Battery"))
+         {
             stepsCount = steps;
-            energyRequired = interactableObject.GetComponent<Battery>().requiredEnergy * stepsCount;
+            energyRequired = item.GetComponent<Battery>().requiredEnergy * stepsCount;
             hasBattery = true;
             print(steps);
-        }
-    }
+         }
+      }
+   }
 
-    public virtual void OnSwitchActivate()
-    {
-        if (canActivate)
-        {
-            if (hasBattery)
-            {
-                steps += stepsCount;
-            }
-            interactableObject.GetComponent<SwitchesInteractableObject>().Activate();
-        }
-    }
+   public virtual void OnSwitchActivate()
+   {
+      if (canActivate)
+      {
+         if (hasBattery)
+         {
+            steps += stepsCount;
+         }
+         foreach (var item in interactableObject)
+         {
+            item.GetComponent<SwitchesInteractableObject>().Activate();
+         }
+      }
+   }
 
 }
