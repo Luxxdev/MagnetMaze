fd500251786ceacb046ca34a0f8c4d4bde99bdfd
Murilo Gama <53795576+Murilovsky@users.noreply.github.com>
2022-11-01 17:55:24 -0300

    Background,HUD e remanejamentos

--- MagnetMaze/Assets/Scripts/DialogTrigger.cs
+++ MagnetMaze/Assets/Scripts/DialogTrigger.cs
@@ -4,32 +4,33 @@ using UnityEngine;
 
 public class DialogTrigger : MonoBehaviour
 {
-    [SerializeField] private GameObject hudObject;
-    private bool textShown = false;
-    // Start is called before the first frame update
-    #region Movement
-    void Start()
-    {
+   [SerializeField] private GameObject hudObject;
+   private bool textShown = false;
+   // Start is called before the first frame update
+   #region Movement
+   void Start()
+   {
 
-    }
-    // Update is called once per frame
-    #endregion
-    void Update()
-    {
+   }
+   // Update is called once per frame
+   #endregion
+   void Update()
+   {
 
-    }
-    // <summary>
-    // Sent when another object enters a trigger collider attached to this
-    // object (2D physics only).
-    // </summary>
-    // <param name="other">The other Collider2D involved in this collision.</param>
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player") && !textShown)
-        {
-            hudObject.GetComponent<BottomTextManagement>().CallDialog();
-            textShown = true;
-        }
-    }
+   }
+   // <summary>
+   // Sent when another object enters a trigger collider attached to this
+   // object (2D physics only).
+   // </summary>
+   // <param name="other">The other Collider2D involved in this collision.</param>
+   void OnTriggerEnter2D(Collider2D other)
+   {
+      if (other.gameObject.CompareTag("Player") && !textShown)
+      {
+         hudObject.GetComponent<BottomTextManagement>().CallDialog();
+         textShown = true;
+         Destroy(this.gameObject);
+      }
+   }
 
 }
