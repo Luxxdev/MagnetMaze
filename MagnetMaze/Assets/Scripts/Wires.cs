e3fedfe863c3d9c5f00a8cb64c392a7a212b6d26
Murilo Gama <53795576+Murilovsky@users.noreply.github.com>
2022-11-02 00:04:51 -0300

    Pequenas mudanças nos níveis e no script dos fios

--- MagnetMaze/Assets/Scripts/Wires.cs
+++ MagnetMaze/Assets/Scripts/Wires.cs
@@ -5,58 +5,58 @@ using UnityEngine;
 public class Wires : SwitchesInteractableObject
 {
 
-    private LineRenderer line;
-    private Dictionary<string, Color> cores = new Dictionary<string, Color>(){
+   private LineRenderer line;
+   private Dictionary<string, Color> cores = new Dictionary<string, Color>(){
         {"positivo",new Color(0.90f, 0.27f, 0.22f,1f)},
         {"negativo",new Color(0.57f, 0.91f, 0.75f,1f)},
         {"desligado",new Color(0.49f, 0.22f, 0.20f,1)},
         {"ligado",new Color(0.81f, 0.46f, 0.17f,1)}
     };
-    [SerializeField] private Mode tipo;
-    [SerializeField] private bool active;
+   [SerializeField] private Mode tipo;
+   [SerializeField] private bool active;
 
-    private enum Mode { onOff, invert };
-    void Start()
-    {
-        line = GetComponent<LineRenderer>();
-        if (tipo == Mode.onOff)
-        {
-            ChangeColor("desligado");
-        }
-        else
-        {
-            ChangeColor("ligado");
-        }
-    }
-    public override void Activate()
-    {
-        if (tipo == Mode.onOff)
-        {
-            if (!active)
-            {
-                ChangeColor("ligado");
-                return;
-            }
+   private enum Mode { onOff, invert };
+   void Start()
+   {
+      line = GetComponent<LineRenderer>();
+      if (tipo == Mode.onOff)
+      {
+         ChangeColor("desligado");
+      }
+      else
+      {
+         ChangeColor("ligado");
+      }
+   }
+   public override void Activate()
+   {
+      if (tipo == Mode.onOff)
+      {
+         if (!active)
+         {
             ChangeColor("desligado");
             return;
-        }
+         }
+         ChangeColor("ligado");
+         return;
+      }
 
-        //   else if (tipo == Mode.invert)
-        //   {
-        //       ChangeColor("ligado");
-        //       // if (!active)
-        //       // {
-        //       //     ChangeColor("negativo");
-        //       //     return;
-        //       // }
-        //       // ChangeColor("positivo");
-        //   }
+      //   else if (tipo == Mode.invert)
+      //   {
+      //       ChangeColor("ligado");
+      //       // if (!active)
+      //       // {
+      //       //     ChangeColor("negativo");
+      //       //     return;
+      //       // }
+      //       // ChangeColor("positivo");
+      //   }
 
-    }
-    public void ChangeColor(string cor)
-    {
-        line.startColor = cores[cor];
-        line.endColor = cores[cor];
-        active = !active;
-    }
+   }
+   public void ChangeColor(string cor)
+   {
+      line.startColor = cores[cor];
+      line.endColor = cores[cor];
+      active = !active;
+   }
 }
