afb835cebfbab34141ab8c6dd6520e65685bed03
Lucas Queirolo dos Santos <lq.santos@hotmail.com>
2022-11-10 20:54:13 -0300

    lvl 4 finished

--- MagnetMaze/Assets/Scripts/Battery.cs
+++ MagnetMaze/Assets/Scripts/Battery.cs
@@ -4,57 +4,60 @@ using UnityEngine;
 
 public class Battery : SwitchesInteractableObject
 {
-   [SerializeField] private GameObject[] interactableObject;
-   [SerializeField] private SpriteRenderer energyBar;
-   public float requiredEnergy = 10;
-   public float energy;
-   public bool isFull = false;
-   public float conectedSwitches = 1;
-   public bool charging = false;
-   public float pressedButtons = 0;
+    [SerializeField] private GameObject[] interactableObject;
+    [SerializeField] private SpriteRenderer energyBar;
+    public float requiredEnergy = 10;
+    public float energy;
+    public bool isFull = false;
+    public float conectedSwitches = 1;
+    public bool charging = false;
+    public float pressedButtons = 0;
 
 
-   public override void Activate()
-   {
-      charging = true;
-      if (!isFull)
-      {
-         enabled = true;
-         energy += Time.deltaTime;
-         energyBar.size += new Vector2(0, 0.04294457f * Time.deltaTime);
-         energy = (energy >= requiredEnergy - 0.1f ? energy = requiredEnergy : energy = energy);
-         if (energy >= requiredEnergy)
-         {
-            foreach (var item in interactableObject)
+    public override void Activate()
+    {
+        charging = true;
+        if (!isFull)
+        {
+            enabled = true;
+            energy += Time.deltaTime;
+            energyBar.size += new Vector2(0, 0.04294457f * Time.deltaTime);
+            if (energy >= requiredEnergy - 0.1)
             {
-               item.GetComponent<SwitchesInteractableObject>().Activate();
+                energy = requiredEnergy;
             }
-            isFull = true;
-            enabled = false;
-         }
-      }
-   }
-   private void Update()
-   {
-      if (pressedButtons < conectedSwitches)
-      {
-         charging = false;
-      }
-      else
-      {
-         charging = true;
-      }
-      if (!charging && !isFull && energy >= (requiredEnergy * pressedButtons / conectedSwitches) + 0.1f)
-      {
-         energy -= Time.deltaTime;
-         energyBar.size -= new Vector2(0, 0.04294457f * Time.deltaTime);
+            if (energy >= requiredEnergy)
+            {
+                foreach (var item in interactableObject)
+                {
+                    item.GetComponent<SwitchesInteractableObject>().Activate();
+                }
+                isFull = true;
+                enabled = false;
+            }
+        }
+    }
+    private void Update()
+    {
+        if (pressedButtons < conectedSwitches)
+        {
+            charging = false;
+        }
+        else
+        {
+            charging = true;
+        }
+        if (!charging && !isFull && energy >= (requiredEnergy * pressedButtons / conectedSwitches) + 0.1f)
+        {
+            energy -= Time.deltaTime;
+            energyBar.size -= new Vector2(0, 0.04294457f * Time.deltaTime);
 
-         if (energy <= 0)
-         {
-            energy = 0;
-            enabled = false;
-         }
-      }
+            if (energy <= 0)
+            {
+                energy = 0;
+                enabled = false;
+            }
+        }
 
-   }
+    }
 }
