41226b3d76e98ab2546994f599407cc463bcb64f
Lucas Queirolo dos Santos <lq.santos@hotmail.com>
2022-12-22 01:28:45 -0300

    gif player done

--- MagnetMaze/Assets/OPaoGameStudio_MagnetMaze/Scripts/ImageHelper.cs
+++ MagnetMaze/Assets/OPaoGameStudio_MagnetMaze/Scripts/ImageHelper.cs
@@ -8,20 +8,21 @@ namespace OPaoGameStudio_MagnetMaze
     public class ImageHelper : MonoBehaviour
     {
         public VideoPlayer selfVideoPlayer;
+        public SpriteRenderer spriteRenderer;
 
         void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag("Player"))
             {
-                gameObject.SetActive(true);
                 selfVideoPlayer.Play();
+                spriteRenderer.enabled = true;
             }
         }
         void OnTriggerExit2D(Collider2D collision)
         {
             if (collision.CompareTag("Player"))
             {
-                gameObject.SetActive(false);
+                spriteRenderer.enabled = false;
                 selfVideoPlayer.Stop();
             }
         }
